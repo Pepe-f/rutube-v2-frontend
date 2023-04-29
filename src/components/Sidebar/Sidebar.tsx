904337794e06d1bdada8cc5e5dc37dc984cdc585
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-04-29 23:50:55 +0300

    Create menu with subscriptions

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,15 +4,37 @@ import { FC } from 'react'
 import Menu from '@/components/Menu/Menu'
 import { menu } from '@/components/Menu/menu.data'
 
+import { useAuth } from '@/hooks/useAuth'
+
+import { apiRtk } from '@/store/api/api'
+
 import styles from './Sidebar.module.scss'
 
 const Sidebar: FC = () => {
+  const { user } = useAuth()
+
+  const { data } = apiRtk.useGetProfileQuery(null, {
+    skip: !user
+  })
+
   return (
     <aside className={styles.sidebar}>
       <Link href='/' className={styles.logo}>
         Rutube 2.0
       </Link>
       <Menu title='Меню' items={menu} />
+      {user && (
+        <Menu
+          title='Мои подписки'
+          items={
+            data?.subscriptions.map(({ toChannel }) => ({
+              image: toChannel.avatarPath,
+              title: toChannel.name,
+              link: `/c/${toChannel.id}`
+            })) || []
+          }
+        />
+      )}
       <div className={styles.copy}>2023 RUTUBE 2.0</div>
     </aside>
   )
